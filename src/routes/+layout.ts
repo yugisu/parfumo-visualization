11a81ca72db11cb55cc5bb0085442a26af47da22
Dmytrii Puzyr <slymahel@gmail.com>
2025-06-13 02:18:57 +0300

    Update trailingSlash

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,6 +2,7 @@ import { redirect } from '@sveltejs/kit';
 import { APP_REDIRECTS } from '../redirects';
 
 export const prerender = true;
+export const trailingSlash = 'always';
 
 export async function load({ url }: any) {
 	const pathname = url.pathname;
