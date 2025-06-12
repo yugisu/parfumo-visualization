86ac72ff1b943a1363d533a3027ee85a4fcf882e
Dmytrii Puzyr <slymahel@gmail.com>
2025-06-13 02:39:04 +0300

    Updates

--- _app/immutable/chunks/B_y_jEVW.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_ph1yxp)==null?void 0:s.base)??"/parfumo-visualization";var a;const e=((a=globalThis.__sveltekit_ph1yxp)==null?void 0:a.assets)??t;export{e as a,t as b};
---
+++ _app/immutable/entry/start.D7-Ls0he.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/2le_xYHO.js";export{o as load_css,r as start};
