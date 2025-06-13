556987cfcb7c3ecae09bca7b20d7e41f94437ee5
Dmytrii Puzyr <slymahel@gmail.com>
2025-06-13 17:28:38 +0300

    Fix redirect

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -5,7 +5,7 @@ export const prerender = true;
 
 export async function load({ url }: any) {
 	const pathname = url.pathname;
-	if (pathname === '/') {
+	if (pathname === `${base}/`) {
 		return redirect(301, `${base}/1337`);
 	}
 }
